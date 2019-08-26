1ebbe4857065c5425136c8c22314ea5489dc35b2
Nima Alikhani <nimajaune@gmail.com>
2019-08-26 16:07:50 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # particle-ratp-display
+
+Ce petit projet a pour but de fournir un affichage rudimentaire avec les horaires du prochain métro / bus / rer.
+
+L'API de Pierre Grimaud est utilisée pour récupérer ces données.
+(https://api-ratp.pierre-grimaud.fr/v4/ , https://github.com/pgrimaud/ratp-api-rest)
+
+## Materiel
+
+J'ai utilisé :
+- une Particle Argon (https://docs.particle.io/argon/)
+- une Adafruit FeatherWing OLED (https://www.adafruit.com/product/2900)
+- une batterie 3.6V 1000mAh
+
+## Photos
+
+A venir
+
+## Installation
+
+L'utilisation des librairies de requêtes https s'étant révélées complexes d'utilisation, j'ai préféré utilisé les webhooks disponibles sur la console Particle.
+
+Voici le custom template utilisé pour récupérer les horaires des métros à la station Place d'Italie sur la ligne 7 :
+```
+{
+    "event": "horaire_ligne7",
+    "responseTopic": "",
+    "errorResponseTopic": "",
+    "url": "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/7/place+d'italie/A",
+    "requestType": "GET",
+    "noDefaults": false,
+    "rejectUnauthorized": true,
+    "responseTemplate": "{{{result.schedules.0.message}}}/{{{result.schedules.1.message}}}"
+}
+```
+
+Une fois les webhook mis en place, il suffit d'utiliser le code. Celui-ci fait appel à la librairie **oled-wing-adafruit** qu'il est peut-être nécessaire d'importer.
+
+## Fonctionnement
+
+A venir
---
+++ code.ino
@@ -0,0 +1,115 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <oled-wing-adafruit.h>
+
+SYSTEM_THREAD(ENABLED);
+OledWingAdafruit display;
+
+short STATE = 0;
+unsigned int nextTime = 0;
+
+//TODO: gestion des problèmes trafic
+void setup() {
+    display.setup();
+	display.clearDisplay();
+	display.display();
+
+    Particle.subscribe("hook-response/horaire_ligne7", myHandler7, MY_DEVICES);
+    Particle.subscribe("hook-response/horaire_185_paris", myHandler185, MY_DEVICES);
+}
+
+
+void loop() {
+    display.loop();
+    if (nextTime > millis()) {
+        if (display.pressedA()) {
+            STATE = 0;
+            nextTime = millis();
+    	}
+        if (display.pressedB()) {
+            STATE = 1;
+            nextTime = millis();
+        }
+        if (display.pressedC()) {
+            STATE = 2;
+            display.clearDisplay();
+            display.display();
+        }
+        return;
+    }
+    
+
+    String data = String(10);
+    if (STATE == 0){
+        Particle.publish("horaire_ligne7", data, PRIVATE);
+    }
+    else if (STATE == 1){
+        Particle.publish("horaire_185_paris", data, PRIVATE);
+    }
+    
+    delay(500);
+    
+    nextTime = millis() + 50000;
+}
+
+
+void myHandler7(const char *event, const char *data) {
+    Particle.publish("Ligne 7 : recu", data, PRIVATE);
+    display_girouette_7();
+    int time1, time2;
+    parse_response(data, &time1, &time2);
+    display_time( time1,  time2) ;   
+}
+
+void myHandler185(const char *event, const char *data) {
+    Particle.publish("Ligne 185 : recu", data, PRIVATE);
+    display_girouette_185();
+    int time1, time2;
+    parse_response(data, &time1, &time2);
+    display_time( time1,  time2) ;   
+}
+
+void parse_response(const char *data, int* time1, int* time2){
+    char *str2, *saveptr2;
+    char *del = "/";
+    char *data2 = strdup(data);
+    int i = 0;
+    for (str2 = data2; ; str2 = NULL) {
+        char *subtoken = strtok_r(str2, del, &saveptr2);
+        if (subtoken == NULL)
+            break;
+        int n=atoi(subtoken);
+        i ? *time2 = n : *time1 = n;
+        i++;
+    }
+}
+
+void display_girouette_7(){
+    display.clearDisplay();
+    display.fillCircle(12,16,12,1);
+	display.setTextColor(BLACK);
+	display.setCursor(7,11);
+	display.setTextSize(2);
+	display.println("7");
+    display.display();
+}
+
+void display_girouette_185(){
+    display.clearDisplay();
+    display.fillRect(1,8,32,17,1);
+	display.setTextColor(BLACK);
+	display.setCursor(0,9);
+	display.setTextSize(2);
+	display.println("185");
+    display.display();
+}
+
+//TODO: Si 2 nombres à 2 chiffres, réduire le deuxième
+void display_time(int time1, int time2){
+    display.setTextColor(WHITE);
+    display.setTextSize(4);
+	display.setCursor(36,2);
+	display.println(String(time1));
+	display.setCursor(80,2);
+	display.println(String(time2));
+	display.display();
+}
